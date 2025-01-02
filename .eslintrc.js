25fc0b47feb0894a629a2dab0556a025ecd13345
Oleg Kizyma <oleg1203@gmail.com>
2025-01-02 17:25:13 +0000

    feat: обновление конфигурации ESLint, Prettier и скриптов в package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,12 @@ module.exports = {
   ],
   plugins: ['@typescript-eslint'],
   rules: {
-    // ...existing code...
+    // ...existing rules...
+    "no-console": 0,
+    "eqeqeq": "warn",
+    "no-cond-assign": 0,
+    "no-unused-vars": 1,
+    "no-extra-semi": "warn",
+    "semi": "warn"
   },
 };
