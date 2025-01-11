fd8560a51fb486ab1f45f81565d91b7771a1821b
Oleg <oleg1203@gmail.com>
2025-01-11 16:55:22 +0000

    feat: видалити застарілі файли та код з проекту

--- tttt/src/utils/logger.js
+++
@@ -1,7 +0,0 @@
-/* global console */
-export default {
-  info: (...args) => console.log('\x1b[36m%s\x1b[0m', ...args),
-  error: (...args) => console.error('\x1b[31m%s\x1b[0m', ...args),
-  success: (...args) => console.log('\x1b[32m%s\x1b[0m', ...args),
-  warn: (...args) => console.warn('\x1b[33m%s\x1b[0m', ...args),
-};
